83abdb567c165d9b28adf15e151ab93037919fd8
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2022-10-22 14:24:37 +0100

    Add files via upload

--- super_root_bytes.py
+++ super_root_bytes.py
@@ -1,8 +1,4 @@
 import sys
-import colorama
-from colorama import Style, Fore
-
-colorama.init()
 
 
 def super_root(num=int, n=float):
@@ -11,14 +7,9 @@ def super_root(num=int, n=float):
 
 def convert_bytes(num):
     """ bytes for humans """
-
     for x in ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB', 'BB', 'GEOPBYTE']:
         if num < 1024.0:
             return str(num)+' '+x
-        elif num == int(1024*1267650600228229401496703205376):
-            return str(num) + ') = (GEOPBYTE' + Style.BRIGHT + Fore.CYAN + '*' + Style.RESET_ALL + '1024'
-        elif num > int(1024*1267650600228229401496703205376):
-            return str(num) + ') = (GEOPBYTE' + Style.BRIGHT + Fore.CYAN + '*' + Style.RESET_ALL + '1024' + Style.BRIGHT + Fore.CYAN + '+!' + Style.RESET_ALL
         num /= 1024.0
 
 
@@ -38,8 +29,8 @@ elif len(sys.argv) == 3:
         int_super_root_bytes = super_root_bytes
         print('')
         print('    [SUPER ROOT BYTES]')
-        print('        [SUPER ROOT BYTES] (' + str(x) + Style.BRIGHT + Fore.CYAN + str('√') + Style.RESET_ALL + str(y) + ' Bytes) = (' + str(x) + Style.BRIGHT + Fore.CYAN + str('√') + Style.RESET_ALL + str(convert_bytes(float(y))) + ') = (' + str(convert_bytes(round(int_super_root_bytes))) + ')')
-        print('        [PRECISION BYTES]  (' + (str(super_root_bytes) + ' Bytes)'))
+        print('        [' + str(convert_bytes(float(super_root_bytes))) + ']')
+        print('        [BYTES]  (' + (str(int(super_root_bytes)) + ' Bytes)'))
         print('')
     else:
         print('invalid argument characters.')
