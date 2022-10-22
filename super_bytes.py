02677cc2e6faa88d90a1e02fdf618b325a776b1f
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2022-10-22 14:02:30 +0100

    Add files via upload

--- super_bytes.py
+++ super_bytes.py
@@ -5,39 +5,27 @@ call_module = False
 
 colorama.init()
 
-b_arg = ['-b', '-k', '-m', '-g', '-t', '-p', '-e', '-z', '-y', '-bb', '-gpb']
 
-
-def convert_bytes(num):
-    """ bytes for humans """
-
-    num = 0
-    for x in ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB', 'BB', 'GEOPBYTE']:
-        if num <= 1024.0:
-            return str(float(num))+' '+str(x)
-        num /= 1024.0
-
-
-def convert_bytes_human(num):
+def convert_bytes(num=float):
     """ bytes for humans """
 
     for x in ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB', 'BB', 'GEOPBYTE']:
+
         if num < 1024.0:
-            return str(num)+' '+x
-        elif num >= int(1267650600228229401496703205376*1024):
-            return str('[MAX]')
+
+            return str(float(num))+' '+str(x)
 
         num /= 1024.0
 
 
-def super_powers(n=int, positive_exponent=int, value=0):
-    """ loop power operation then multiply result to value=n """
+def super_powers(n=int, positive_exponent=int, quantity=0):
+    """ loop power operation then multiply result to quantity=n """
     i = 1
     super_power = float(n)
     while i < positive_exponent:
         super_power = float(float(super_power) * float(n))
         i += 1
-    return float(super_power), float(super_power*value)
+    return float(super_power), float(super_power*quantity)
 
 
 if '-h' in sys.argv:
@@ -60,33 +48,6 @@ if '-h' in sys.argv:
     print('')
     print('        [AUTHOR] Written by Benjamin Jack Cullen.')
 
-elif sys.argv[1] in b_arg:
-    """ ['-k', '-m', '-g', '-t', '-p', '-e', '-z', '-y', '-bb', '-gpb']: """
-
-    positive_exponent_ = [0.0,
-                          1.0, 2.0, 3.0, 4.0, 5.0,
-                          6.0, 7.0, 8.0, 9.0, 10]
-
-    positive_exponent = positive_exponent_[0]
-
-    idx = 0
-    n_ = 0
-
-    for _ in b_arg:
-        if sys.argv[1] == _:
-            print('-- matching sys.argv:', _)
-            idx = sys.argv.index(_)
-    positive_exponent = positive_exponent_[idx]
-
-    print('')
-    print('    [SUPER POWER BYTES]')
-    value = int(sys.argv[idx+1])
-
-    sp = super_powers(n=int(1024), positive_exponent=int(positive_exponent), value=value)
-    str_super_power = str(sp[0])
-    int_super_power = int(sp[0])
-    str_human_super_power = str(convert_bytes(int(int_super_power)))
-
 
 elif len(sys.argv) >= 3:
     """ Allow arbitrary bytes, powers, values. """
@@ -94,47 +55,53 @@ elif len(sys.argv) >= 3:
     print('')
     print('    [SUPER POWER BYTES]')
 
+    # check n
     n_ = 1
     if sys.argv[1].isdigit():
         n_ = int(sys.argv[1])
-    positive_exponent = 1
 
+    # check exponent
+    positive_exponent = 1
     if sys.argv[2].isdigit():
         positive_exponent = int(sys.argv[2])
 
-    value = 1
+    # check quantity
+    quantity = 1.0
     if len(sys.argv) == 4:
         if sys.argv[3].isdigit():
-            value = int(sys.argv[3])
+            quantity = int(sys.argv[3])
 
-    sp = super_powers(n=int(n_), positive_exponent=int(positive_exponent), value=value)
+    # power n by exponent and multiply by quantity
+    sp = super_powers(n=int(n_), positive_exponent=int(positive_exponent), quantity=quantity)
     str_super_power = str(sp[0])
-    int_super_power = int(sp[0])
-    str_human_super_power = str(convert_bytes(int(int_super_power)))
+    int_super_power = float(sp[0])
+    str_human_super_power = str(convert_bytes(float(int_super_power)))
 
-    """ Allow if super_powered number seemingly n bytes """
+    """ < 1024 """
     if sp[0] < 1024:
         str_head = '        [BYTES] (' + str('1') + Style.BRIGHT + Fore.CYAN + '^' + Style.RESET_ALL
         str_ex = str(positive_exponent)
-        str_val = ') * ' + str(value) + ') = ('
-        str_tails = str(float(value)) + ' * Bytes )'
-
+        str_val = ') * ' + str(quantity) + ') = ('
+        ap = ' Byte )'
+        if quantity > 1:
+            ap = ' Bytes )'
+        str_tails = str(int(quantity)) + ap
+        str_human = '        [' + str(convert_bytes(quantity)) + ']'
+        print(str_human)
         print(str_head,
               str_ex,
               str_val,
               str_tails)
 
     else:
-        """ Allow if super_powered number greater than bytes """
+        """ > 1024 """
         str_head = '        [BYTES] (' + str('1024') + Style.BRIGHT + Fore.CYAN + '^' + Style.RESET_ALL
         str_ex = str(positive_exponent)
-        str_val = ') * ' + str(value) + ') = ('
-        str_tails = str(int(sp[1])) + ' * Bytes )'
-
-        int_tails = int(int(sp[1]))
-        str_human = '        [' + str(convert_bytes_human(int_tails)) + ']'
+        str_val = ') * ' + str(quantity) + ') = ('
+        str_tails = str(int(sp[1])) + ' Bytes )'
+        int_tails = int(sp[1])
+        str_human = '        [' + str(convert_bytes(int_tails)) + ']'
         print(str_human)
-
         print(str_head,
               str_ex,
               str_val,
